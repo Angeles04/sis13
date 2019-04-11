9f308c271f1889e4d020213a3e7c2173c88c462b
Administrador <Administrador@172.16.17.126>
2019-04-11 15:37:29 -0600

    ejercicio2

--- ejercicios/ejercicio2.php
+++ ejercicios/ejercicio2.php
@@ -1,7 +1,10 @@
 <?php
         $suma=0;
+        $suma1=0;
         for($i=1; $i <= 100; $i++){
+            echo ''.$suma."<br/>";
             $suma = $suma + $i;
         }
         echo "Total: ".$suma;
-?>
+
+?> 
