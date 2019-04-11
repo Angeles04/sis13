e46f747112ea8e93f2914a0b2156ebb1a9488dd8
Administrador <Administrador@172.16.17.126>
2019-04-11 15:10:32 -0600

    ejercicio2

--- ejercicios/ejercicio2.php
+++ ejercicios/ejercicio2.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+        $suma=0;
+        for($i=1; $i <= 100; $i++){
+            $suma = $suma + $i;
+        }
+        echo "Total: ".$suma;
+?>
